e5576bec240aed8aa702f8489482fd5b17f60014
Alexander Lee <alex.lee2@kellogg.northwestern.edu>
2026-02-10 04:52:52 +0000

    Second update

--- kmdb.rb
+++ kmdb.rb
@@ -75,6 +75,38 @@
 # ====================
 # Christian Bale
 
+# ======================== My output ========================
+# Movies
+# ======
+
+# Batman Begins          2005 PG-13  Warner Bros.
+# The Dark Knight        2008 PG-13  Warner Bros.
+# The Dark Knight Rises  2012 PG-13  Warner Bros.
+
+# Top Cast
+# ========
+
+# Batman Begins          Christian Bale       Bruce Wayne
+# Batman Begins          Michael Caine        Alfred
+# Batman Begins          Liam Neeson          Ra's Al Ghul
+# Batman Begins          Katie Holmes         Rachel Dawes
+# Batman Begins          Gary Oldman          Commissioner Gordon
+# The Dark Knight        Christian Bale       Bruce Wayne
+# The Dark Knight        Heath Ledger         Joker
+# The Dark Knight        Aaron Eckhart        Harvey Dent
+# The Dark Knight        Michael Caine        Alfred
+# The Dark Knight        Maggie Gyllenhaal    Rachel Dawes
+# The Dark Knight Rises  Christian Bale       Bruce Wayne
+# The Dark Knight Rises  Gary Oldman          Commissioner Gordon
+# The Dark Knight Rises  Tom Hardy            Bane
+# The Dark Knight Rises  Joseph Gordon-Levitt John Blake
+# The Dark Knight Rises  Anne Hathaway        Selina Kyle
+
+# Represented by agent
+# ====================
+
+# Christian Bale
+
 # My Summary
 
 # Delete Data	
